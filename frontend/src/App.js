2913fd947278fab1c08da3f5a0fe96fd634151e7
AaronElam <speztra97@gmail.com>
2021-10-07 21:09:43 -0500

    Create frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import MovieTable from "./components/MovieTable";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MovieTable />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/MovieTable.js
@@ -0,0 +1,7 @@
+import Table from "react-bootstrap/Table";
+
+const getMovies = () => {};
+
+const MovieTable = ({ movies }) => {};
+
+export default MovieTable;

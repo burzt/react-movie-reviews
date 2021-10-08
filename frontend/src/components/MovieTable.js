1fbb25b7b9f8d7fd03fb605800bbeb4f572255e1
AaronElam <speztra97@gmail.com>
2021-10-07 23:08:01 -0500

    Client/server communication

--- frontend/src/components/MovieTable.js
+++ frontend/src/components/MovieTable.js
@@ -1,7 +1,37 @@
+import { useState, useEffect, useCallback } from "react";
 import Table from "react-bootstrap/Table";
+import axios from "axios";
 
-const getMovies = () => {};
+const MovieTable = () => {
+  const [data, setData] = useState("");
 
-const MovieTable = ({ movies }) => {};
+  // Using axios to fetch data from the API
+  const fetchData = useCallback(async () => {
+    axios.get("http://localhost:4000").then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
+  // Using useEffect to update UI when data changes
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          {/* <th>Updoots</th> */}
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{data.exampleMessage}</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+};
 
 export default MovieTable;
